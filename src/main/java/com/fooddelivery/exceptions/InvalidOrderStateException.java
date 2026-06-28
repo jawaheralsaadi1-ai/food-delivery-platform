bd105ed73f574dba08d56f8b727d335eef7d9f19
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-28 15:45:07 +0400

    add(exceptions): refactor InvalidOrderStateException class.

--- src/main/java/com/fooddelivery/exceptions/InvalidOrderStateException.java
+++ src/main/java/com/fooddelivery/exceptions/InvalidOrderStateException.java
@@ -1,7 +1,21 @@
 package com.fooddelivery.exceptions;
 
 public class InvalidOrderStateException extends RuntimeException {
-    public InvalidOrderStateException(String message) {
-        super(message);
+
+    private final String currentState;
+    private final String attemptedAction;
+
+    public InvalidOrderStateException(String currentState, String attemptedAction) {
+        super(String.format("Cannot %s order in state: %s", attemptedAction, currentState));
+        this.currentState = currentState;
+        this.attemptedAction = attemptedAction;
+    }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+
+    public String getAttemptedAction() {
+        return attemptedAction;
     }
 }
