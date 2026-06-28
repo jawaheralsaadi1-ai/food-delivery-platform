8b4fcf9eec224b75a49d68cb5cf86f31d51b5306
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-28 15:59:20 +0400

    add(dto/response): implement ErrorResponseDTO class.

--- src/main/java/com/fooddelivery/dto/response/ErrorResponseDTO.java
+++ src/main/java/com/fooddelivery/dto/response/ErrorResponseDTO.java
@@ -1,4 +1,19 @@
 package com.fooddelivery.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) //
 public class ErrorResponseDTO {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+    private Map<String, String> fieldErrors; //
 }
