ea5d798d9e4cea174941a6591892495b6139123b
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-28 16:08:02 +0400

    fix(exceptions): fix Expected 3 arguments but found 1 by fix ResourceNotFoundException .

--- src/main/java/com/fooddelivery/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/fooddelivery/exceptions/ResourceNotFoundException.java
@@ -2,26 +2,11 @@ package com.fooddelivery.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-    private final String resourceName;
-    private final String fieldName;
-    private final Object fieldValue;
-
-    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public Object getFieldValue() {
-        return fieldValue;
+    public ResourceNotFoundException(String resourceName, Object id) {
+        super(String.format("%s with ID '%s' not found or has been deactivated", resourceName, id));
     }
 }
